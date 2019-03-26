23a380e46d05a6e07d94b56719c3db246e7179e3
Jeff Labonte <jeff.labonte12@gmail.com>
2019-03-25 21:23:30 -0400

    Put in place inital structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open("README.md", 'r') as f:
+    readme = f.read()
+
+setup(
+    name="libboutique",
+    version="0.1.0",
+    description="Software Boutique's management for curated applications with interfaces for Apt, Snapd and others.",
+    long_description=readme,
+    author="Jeff Labonté",
+    author_email="jeff.labonte12@gmail.com",
+    url="https://github.com/ubuntu-mate/python3-boutique",
+    packages=find_packages(exclude=('tests', 'docs'))
+)
---
+++ software-boutique-cli.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+import argparse
+
+
+if __name__ == "__main__":
+    pass
