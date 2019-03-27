9cb2298abe35f822ebd8373d478630877666ed02
Jeff Labonte <jeff.labonte12@gmail.com>
2019-03-26 21:01:51 -0400

    First iteration of install, retrieve package by name, and extract data
    I believe that extract data might be useful to facilitate transfert of information to the front-end later on. Install works but still need to add callback, I am planning to provide ProgressCallback into the constructor, so it could be easier to give you a callback ( just a thought ). Able to find a snap using the name

--- libboutique/snap/snap_service.py
+++ libboutique/snap/snap_service.py
@@ -1,16 +1,38 @@
 import gi
 import os
 import json
-try:
-    gi.require_version("Snapd", 1)
-    from gi.repository import Snapd
-except:
-    pass
 
-class SnapService(object):
+gi.require_version("Snapd", '1')
+from gi.repository import Snapd
+
+from libboutique.common.base_package_service import BasePackageService
+
+class SnapService(BasePackageService):
 
     def __init__(self):
+        self.snap_client = Snapd.Client().new()
+        self.channel = "stable"
+        self.package_type = "snap"
+
+    def install_package(self, name):
+        snap = self.snap_client.list_one_sync(name=name)
+        return self.snap_client.install2_sync(flags=0, name=name, channel=self.channel, revision=snap.get_revision())
+
+    def remove_package(self, name):
         pass
 
-    def _connect(self):
-        pass
+    def retrieve_package_information_by_name(self, name):
+        snap = self.snap_client.list_one_sync(name=name)
+        return self._extract_snap_to_dict(snap=snap)
+
+    def _extract_snap_to_dict(self, snap):
+        return {
+            "id": snap.get_id(),
+            "name": snap.get_name(),
+            "type": self.package_type,
+            "revision": snap.get_revision(),
+            "license": snap.get_license(),
+            "icon": snap.get_icon(),
+            "download_size": snap.get_download_size(),
+            "install_date": snap.get_install_date()
+        }
