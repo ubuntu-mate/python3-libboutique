ebc1d98e101ce245070818ee915c0886d75ea3a6
Jeff Labonte <jeff.labonte12@gmail.com>
2019-03-28 21:44:17 -0400

    Add arguments for the cli

--- software-boutique-cli.py
+++ software-boutique-cli.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-
 import argparse
+
+from libboutique.snap.snap_service import SnapService
 
 
 if __name__ == "__main__":
-    pass
+    args = argparse.ArgumentParser()
+    groups = args.add_mutually_exclusive_group()
+    groups.add_argument('--install-app=', type=str, help="Install specified package")
+    groups.add_argument('--remove-app=', type=str, help="Remove specified package")
+    groups.add_argument("--query=", type=str, help="Return list of available package") # TODO Work on the wording
+    package_name = args.parse_args()
