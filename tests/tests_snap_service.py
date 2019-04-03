d138ace20e087ec49c80194e8252a7accd995349
Jeff Labonte <jeff.labonte12@gmail.com>
2019-04-02 23:48:39 -0400

    Add remaining test, add verbose to test

--- tests/tests_snap_service.py
+++ tests/tests_snap_service.py
@@ -4,12 +4,24 @@ from unittest.mock import Mock, patch
 
 gi.require_version('Snapd', '1')
 from gi.repository import Snapd
+from gi.repository import GLib
 
 from libboutique.services.snap.snap_service import SnapService
 
 
 class TestSnapService(unittest.TestCase):
     """TestSnapService"""
+
+    def validatePackageInformationDict(self, package):
+        self.assertIsInstance(package, dict)
+        self.assertNotEqual(package.get("id", None), None)
+        self.assertNotEqual(package.get("name", None), None)
+        self.assertEqual(package.get("type", None), "snap")
+        self.assertNotEqual(package.get("revision", None), None)
+        self.assertNotEqual(package.get("license", 1), 1)
+        self.assertNotEqual(package.get("icon", 1), 1)
+        self.assertNotEqual(package.get("download_size", None), None)
+        self.assertNotEqual(package.get("install_date", 1), 1)
 
     def testInstallPackage(self):
         """testInstallPackage"""
@@ -63,6 +75,17 @@ class TestSnapService(unittest.TestCase):
         self.assertEqual(result.get("message"), 'snap "bw" is not installed')
         self.assertEqual(result.get("args"), ('snap "bw" is not installed',))
         self.assertEqual(result.get("domain"), "snapd-error-quark")
+
+    def testRetrieveInstalledPackage(self):
+        snap_service = SnapService(progress_publisher=None)
+        list_installed_packages = snap_service.get_installed_package()
+        self.assertIsInstance(list_installed_packages, list)
+        for package in list_installed_packages:
+            self.validatePackageInformationDict(package=package)
+
+    def testRetrieveInformationPackage(self):
+        snap_service = SnapService(progress_publisher=None)
+        package_info = snap_service.retrieve_package_information_by_name(name="bw")
 
 
 if __name__ == "__main__":
