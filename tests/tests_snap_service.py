a15137f6d8cd41462f1e209712b727693d2330e2
Jeff Labonte <jeff.labonte12@gmail.com>
2019-04-20 16:43:44 -0400

    Fix tests to handle new package_format

--- tests/tests_snap_service.py
+++ tests/tests_snap_service.py
@@ -12,18 +12,22 @@ from libboutique.services.snap.snap_service import SnapService
 class TestSnapService(unittest.TestCase):
     """TestSnapService"""
 
-    def validatePackageInformationDict(self, package):
+    def validate_package_information_dict(self, package):
         self.assertIsInstance(package, dict)
         self.assertNotEqual(package.get("id", None), None)
         self.assertNotEqual(package.get("name", None), None)
-        self.assertEqual(package.get("type", None), "snap")
-        self.assertNotEqual(package.get("revision", None), None)
-        self.assertNotEqual(package.get("license", 1), 1)
-        self.assertNotEqual(package.get("icon", 1), 1)
-        self.assertNotEqual(package.get("download_size", None), None)
-        self.assertNotEqual(package.get("install_date", 1), 1)
+        self.assertEqual(package.get("package_type", None), "snap")
+        self.assertNotEqual(package.get("version", None), None)
+        self.assertNotEqual(package.get("license", 1), 1) # The value expected is None
+        self.assertNotEqual(package.get("is_installed", None), None)
+        self.assertNotEqual(package.get("installed_date", 1), 1)
+        self.assertEqual(package.get("distro", None), "ubuntu")
+        self.assertNotEqual(package.get("version_installed", 1), 1)
+        self.assertNotEqual(package.get("platform", 1), 1)
+        self.assertNotEqual(package.get("price", None), None)
+        self.assertNotEqual(package.get("summary", None), None)
 
-    def testInstallPackage(self):
+    def test_install_package(self):
         """testInstallPackage"""
         with patch.object(Snapd.Client, "new", return_value=Mock()) as snap_client_mock:
             publisher_mock = Mock()
@@ -36,7 +40,7 @@ class TestSnapService(unittest.TestCase):
                                                               channel="stable",
                                                               progress_callback=snap_service.progress_callback)
 
-    def testRemovePackage(self):
+    def test_remove_package(self):
         """testRemovePackage"""
         with patch.object(Snapd.Client, "new", return_value=Mock()) as snap_client_mock:
             publisher_mock = Mock()
@@ -47,7 +51,7 @@ class TestSnapService(unittest.TestCase):
             snap_client.remove_sync.assert_called_once_with(name="bw",
                                                             progress_callback=snap_service.progress_callback)
 
-    def testInstallPackageTwice(self):
+    def test_install_package_twice(self):
         """testInstallPackageTwice"""
         snap_service = SnapService(progress_publisher=None)
         snap_service.install_package(name="bw")
@@ -57,7 +61,7 @@ class TestSnapService(unittest.TestCase):
         self.assertEqual(result.get("args"), ('snap "bw" is already installed',))
         self.assertEqual(result.get("domain"), "snapd-error-quark")
 
-    def testInstallNewPackage(self):
+    def test_install_new_package(self):
         """testInstallNewPackage"""
         snap_service = SnapService(progress_publisher=None)
         snap_service.remove_package(name="bw")
@@ -66,7 +70,7 @@ class TestSnapService(unittest.TestCase):
         self.assertEqual(result.get("message"), "success")
         self.assertEqual(result.get("name"), "bw")
 
-    def testRemoveTwicePackage(self):
+    def test_remove_package_twice(self):
         """testRemoveTwicePackage"""
         snap_service = SnapService(progress_publisher=None)
         snap_service.remove_package(name="bw")
@@ -76,16 +80,18 @@ class TestSnapService(unittest.TestCase):
         self.assertEqual(result.get("args"), ('snap "bw" is not installed',))
         self.assertEqual(result.get("domain"), "snapd-error-quark")
 
-    def testRetrieveInstalledPackage(self):
+    def test_retrieve_installed_package(self):
         snap_service = SnapService(progress_publisher=None)
         list_installed_packages = snap_service.get_installed_package()
         self.assertIsInstance(list_installed_packages, list)
         for package in list_installed_packages:
-            self.validatePackageInformationDict(package=package)
+            self.validate_package_information_dict(package=package)
 
-    def testRetrieveInformationPackage(self):
+    def test_retrieve_package_information(self):
         snap_service = SnapService(progress_publisher=None)
         package_info = snap_service.retrieve_package_information_by_name(name="bw")
+        for package in package_info:
+            self.validate_package_information_dict(package=package)
 
 
 if __name__ == "__main__":
