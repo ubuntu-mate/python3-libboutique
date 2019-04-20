2699cd8396081b48b4118727183636f1e1f898f6
Jeff Labonte <jeff.labonte12@gmail.com>
2019-04-20 15:07:51 -0400

    Update the formatter
    Change the name of some arguments, add package_id, set some of the arguments default values, add some documentation in the pydoc to make it easier to understand

--- libboutique/formatter/package_formatter.py
+++ libboutique/formatter/package_formatter.py
@@ -3,32 +3,36 @@ class PackageFormatter():
         pass
 
     @staticmethod
-    def format_package_informations(name, dev, icon, summary, platform, source,
-                                    package_type, dependencies, version,
-                                    is_installed, version_installed, distro):
+    def format_package_informations(id_package, name, platform, source, package_type,
+                                    version, is_installed, dev_name=None, icon=None,
+                                    media=None, summary=None, dependencies=None, version_installed=None,
+                                    installed_date=None, distro="ubuntu", license=None):
         """format_package_informations
 
-        Information for the front-end - Those informations
-        should be provided back to the backend so it can be
-        installed.
-
+        Build data structure to communicate from
+        the back-end to the front-end and vice-versa
+        :param id_package: str/int
         :param name: str
-        :param dev: str
+        :param dev_name: str default None
         :param icon: str
         :param summary: str
         :param platform: str
-        :param source: str
-        :param package_type: str
-        :param dependencies: str
+        :param source: str : ppa/distro repo/snap
+        :param package_type: str deb/appImage/snap/flatpak (if we use flatpak one day)
+        :param dependencies: str default None
         :param version: str i.e 1.1.2-ubuntu
         :param is_installed: bool
-        :param version_installed: str
-        :param distro: str i.e ubuntu
+        :param version_installed: str default None
+        :param installed_date: datetime default None
+        :param distro: str default "ubuntu"
+        :param license: str default None
         """
         return {
+            "id": id_package,
             "name": name,
-            "dev_name": dev,
+            "dev_name": dev_name,
             "icon_path": icon,
+            "media": media,
             "summary": summary,
             "platform": platform,
             "source": source,
@@ -37,5 +41,7 @@ class PackageFormatter():
             "version": version,
             "is_installed": is_installed,
             "version_installed": version_installed,
-            "distro": distro
+            "installed_date": installed_date,
+            "distro": distro,
+            "license": license
         }
