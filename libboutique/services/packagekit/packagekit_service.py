6d09c3243c4a138c46c755eafb1acf073fbfe145
Jeff Labonte <jeff.labonte12@gmail.com>
2019-04-21 22:59:42 -0400

    Document the code with PyDoc
    This should help me or other see the progress of the development and make it easier to come back to code

--- libboutique/services/packagekit/packagekit_service.py
+++ libboutique/services/packagekit/packagekit_service.py
@@ -33,16 +33,55 @@ class PackageKitService(BasePackageService):
         pass
 
     def retrieve_package_information_by_name(self, name):
+        """retrieve_package_information_by_name
+
+        Retrieve all available informations that can be taken
+        from the package manager ( in our case apt) from a partial
+        or complete name
+
+        :param name: str
+        :returns : list of dictionnaries with all the informations in the Package
+        """
         search_results = self.packagekit_client.search_details(filters=1, values=[name,], cancellable=None, progress_callback=self.progress_callback, progress_user_data=None)
         return self._create_dict_from_array(search_results.get_package_array())
 
     def _create_dict_from_array(self, package_array):
+        """_create_dict_from_array
+
+        extract data from each Package found
+
+        :param package_array: [ PackageKitGlib.Package, ...]
+        :returns : list of dictionnaries with all the informations in the Package
+        """
         packages = []
         for package in package_array:
             packages.append(self._extract_package_to_dict(package=package))
         return packages
 
     def _extract_package_to_dict(self, package):
+        """_extract_package_to_dict
+
+        Use the formatter to return a dictionnary
+        filled all the informations found in the Package object
+
+        :param package: PackageKitGlib.Package
+        :returns dict: Dictionnary of the information in the Package
+        """
         return PackageFormatter.format_package_informations(id_package=package.get_id(), name=package.get_name(),
-         platform=package.get_arch(), summary=package.get_summary(), source="apt", package_type="apt", 
+         platform=package.get_arch(), summary=package.get_summary(), source=package.get_data(), package_type="apt",
          version=package.get_version(), is_installed=package.get_data())
+
+    def __extract_information_from_strings(self, package):
+        """__extract_information_from_strings
+
+            Some Package informations are stored in
+            strings or Enums. This function
+            has for purpose to extract those embedded informations
+
+        :doc: https://lazka.github.io/pgi-docs/#PackageKitGlib-1.0/classes/Package.html#PackageKitGlib.Package
+        :param package:
+        :return TBD -> TODO find out what can be extracted
+        """
+        # TODO Extract data from the Package Data
+        pass
+
