ad9380e398a7a8d75295761adabbd8e42080de5c
Jeff Labonte <jeff.labonte12@gmail.com>
2019-08-20 00:36:30 -0400

    Restructure BasePackageService, Add base method for extracting properties from packages
    Add commons to all package services to BasePackageService

--- libboutique/services/common/base_package_service.py
+++ libboutique/services/common/base_package_service.py
@@ -1,18 +1,39 @@
-from typing import List
+from typing import Dict, List
+
+import distro
 
 
 class BasePackageService:
-    def install_package(self, name):
+    """
+        Base class for each Package Services i.e PackageKit and Snap
+    """
+
+    def __init__(self, progress_publisher=None):
+        self.package_type = "Unknown"
+        self.distribution = " ".join(distro.linux_distribution(full_distribution_name=False)[0:2])
+        self.progress_publisher = progress_publisher
+
+    def install_package(self, name: str):
         raise NotImplementedError("You must implement it in your class")
 
-    def remove_package(self, name):
+    def remove_package(self, name: str):
         raise NotImplementedError("You must implement it in your class")
 
-    def retrieve_package_information_by_name(self, name):
+    def retrieve_package_information_by_name(self, name: str):
         raise NotImplementedError("You must implement it in your class")
 
     def list_installed_packages(self) -> List:
         raise NotImplementedError("You must implement it in your class")
+
+    def _extract_package_to_dict(self, package) -> Dict:
+        return {
+            "package_id": package.get_id(),
+            "name": package.get_name(),
+            "distribution": self.distribution,
+            "version": package.get_version(),
+            "source": self.package_type,
+            "summary": package.get_summary(),
+        }
 
     def _format_glib_error(self, exception):
         return {
