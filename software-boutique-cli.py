012cff9f62c77d9fc9c53e99f437b5ee0699daf2
Jeff Labonte <jeff.labonte12@gmail.com>
2019-08-20 00:45:18 -0400

    Run Black to reformat

--- software-boutique-cli.py
+++ software-boutique-cli.py
@@ -4,10 +4,10 @@ import argparse
 from libboutique.publisher.progress_publisher import ProgressPublisher
 from libboutique.handler.command_handler import CommandHandler
 
+
 def show_progress(package, progress):
     # print(package)
     print(progress)
-
 
 
 if __name__ == "__main__":
@@ -16,19 +16,19 @@ if __name__ == "__main__":
     commands = {
         "install_app=": command_handler.install_package,
         "remove_app=": command_handler.remove_package,
-        "query=": None
+        "query=": None,
     }
 
     args = argparse.ArgumentParser()
     groups = args.add_mutually_exclusive_group()
-    groups.add_argument('--install-app=', type=str, help="Install specified package")
-    groups.add_argument('--remove-app=', type=str, help="Remove specified package")
-    groups.add_argument("--query=", type=str, help="Return list of available package") # TODO Work on the wording
+    groups.add_argument("--install-app=", type=str, help="Install specified package")
+    groups.add_argument("--remove-app=", type=str, help="Remove specified package")
+    groups.add_argument("--query=", type=str, help="Return list of available package")  # TODO Work on the wording
     cli_input = args.parse_args()
 
     cli_dict = vars(cli_input)
 
-    for key,value in cli_dict.items():
+    for key, value in cli_dict.items():
         if value is not None:
             commands.get(key)(name=value)
             break
