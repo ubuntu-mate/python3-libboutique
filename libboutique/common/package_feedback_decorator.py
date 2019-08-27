941ca5b3121a10bbea8d686482d5301f634ba8e2
Jeff Labonte <jeff.labonte12@gmail.com>
2019-08-26 22:25:39 -0400

    Create a decorator to handle feedback and logging
    Decorator returns a dict with a success or a faillure to the user. Add event logging inside the decorator to make debugging easier later

---
+++ libboutique/common/package_feedback_decorator.py
@@ -0,0 +1,35 @@
+import logging
+from functools import wraps
+
+
+def format_glib_error(exception):
+    return {
+        "args": exception.args,
+        "code": exception.code,
+        "domain": exception.domain,
+        "message": exception.message,
+    }
+
+
+def successful_message(action, arguments):
+    return {"action": action, "arguments": arguments, "message": "success"}
+
+
+class TransactionFeedbackDecorator:
+
+    def __init__(self, action):
+        self.action = action
+
+    def __call__(self, fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            try:
+                fn(*args, **kwargs)
+                logging.info("Success: {self.action} - {args} & {kwargs}".format(**locals()))
+                return successful_message(action=self.action, arguments=(args, kwargs))
+            except Exception as ex:
+                logging.exception("Error: {self.action} - {args} & {kwargs}: {ex}".format(**locals()))
+                return format_glib_error(exception=ex)
+        return wrapper
+
+
