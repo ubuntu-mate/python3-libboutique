4c5b3f6cfb6b11cfc23549f0e686cd2673d26927
Jeff Labonte <jeff.labonte12@gmail.com>
2019-09-19 20:52:27 -0400

    Update CommonServiceTests
    Remove the minute to be asserted, since it doesn't have much value in this case. Add a common test to make sure there is no installation_dates for a package that was removed. Change signature to have a package_service instead of just the install_date object

--- tests/common_service_tests.py
+++ tests/common_service_tests.py
@@ -1,20 +1,27 @@
 from datetime import datetime
 
-from libboutique.database.models import InstallationDates
+from libboutique.services.common.base_package_service import BasePackageService
 
 
 class CommonServiceTests:
     PACKAGE_TYPE = "Unknown"
 
     @classmethod
-    def assert_installation_date(cls, installation_date: InstallationDates, expected_package_name: str) -> None:
+    def assert_installation_date(cls, package_service: BasePackageService, expected_package_name: str) -> None:
         """
             Assert installation dates
         """
+        installation_date = package_service.get_package_installation_date_by_package_name(package_name=expected_package_name)
         assert installation_date.package_type == cls.PACKAGE_TYPE
         assert installation_date.package_name == expected_package_name
         assert installation_date.installation_datetime.year == datetime.now().year
         assert installation_date.installation_datetime.month == datetime.now().month
         assert installation_date.installation_datetime.day == datetime.now().day
-        assert installation_date.installation_datetime.hour == datetime.now().hour
-        assert installation_date.installation_datetime.minute == datetime.now().minute
+
+    @classmethod
+    def assert_no_installation_date(cls, package_service: BasePackageService, expected_package_name) -> None:
+        """
+            Assert that there is nothing in the database
+        """
+        assert package_service.get_package_installation_date_by_package_name(expected_package_name) is None
+
