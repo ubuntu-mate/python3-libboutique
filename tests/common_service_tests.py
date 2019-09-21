1d1595992e679c3ab45ee2e8d44c4dd969c251d0
Jeff Labonte <jeff.labonte12@gmail.com>
2019-09-20 20:51:28 -0400

    Update CommonServiceTests
    Call datetime.now() only once and store it in a variable called now. Add hours to assertion in assert_installation_date

--- tests/common_service_tests.py
+++ tests/common_service_tests.py
@@ -12,11 +12,13 @@ class CommonServiceTests:
             Assert installation dates
         """
         installation_date = package_service.get_package_installation_date_by_package_name(package_name=expected_package_name)
+        now = datetime.now()
         assert installation_date.package_type == cls.PACKAGE_TYPE
         assert installation_date.package_name == expected_package_name
-        assert installation_date.installation_datetime.year == datetime.now().year
-        assert installation_date.installation_datetime.month == datetime.now().month
-        assert installation_date.installation_datetime.day == datetime.now().day
+        assert installation_date.installation_datetime.year == now.year
+        assert installation_date.installation_datetime.month == now.month
+        assert installation_date.installation_datetime.day == now.day
+        assert installation_date.installation_datetime.hour == now.hour
 
     @classmethod
     def assert_no_installation_date(cls, package_service: BasePackageService, expected_package_name) -> None:
