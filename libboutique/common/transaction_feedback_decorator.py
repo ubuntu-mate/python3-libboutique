fc0f8e3571f906639660846d93901c8eeb928a31
Jeff Labonte <jeff.labonte12@gmail.com>
2019-10-20 17:12:21 -0400

    Update doc of transaction_feedback_decorator

--- libboutique/common/transaction_feedback_decorator.py
+++ libboutique/common/transaction_feedback_decorator.py
@@ -1,18 +1,28 @@
 import logging
+from typing import Dict, Any, Tuple
 from functools import wraps
 
 from libboutique.common.transaction_actions import TransactionActionsEnum
 
 
-def format_glib_error(exception):
+def format_glib_error(exception) -> Dict:
     return {"args": exception.args, "code": exception.code, "domain": exception.domain, "message": exception.message}
 
 
-def successful_message(action, arguments):
-    return {"action": action, "arguments": arguments, "message": "success"}
+def successful_message(action: TransactionActionsEnum, arguments: Tuple, result: Any) -> Dict:
+    return {"action": action, "arguments": arguments, "message": "success", "result": result}
 
 
 class transaction_feedback_decorator:
+    """
+        ******************************
+        *        **DECORATOR**       *
+        ******************************
+
+        Unify the response structure for every service
+        supported
+    """
+
     def __init__(self, action: TransactionActionsEnum):
         self.action = action.value
 
