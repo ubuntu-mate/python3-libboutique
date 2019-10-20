9291b2f7b6a0be9c241307f138303205bc87405c
Jeff Labonte <jeff.labonte12@gmail.com>
2019-10-20 17:00:58 -0400

    Add new action, update LIST_INSTALLED

--- libboutique/common/transaction_actions.py
+++ libboutique/common/transaction_actions.py
@@ -5,6 +5,7 @@ class TransactionActionsEnum(Enum):
     INSTALL = "install"
     REMOVE = "remove"
     SEARCH = "search"
-    LIST_INSTALLED = "list"
+    LIST_INSTALLED = "list_installed"
+    LIST_INSTALLED_REPOS = "list_installed_repos"
     REFRESH_CACHE = "refresh_cache"
     REPAIR = "dpkg_repair"
