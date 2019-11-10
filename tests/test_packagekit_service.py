5bd814ef5f31848b5294d8c9cb6c246fa1c04030
Jeff Labonte <jeff.labonte12@gmail.com>
2019-11-09 20:27:55 -0500

    Restructure unittests - Repeat less

--- tests/test_packagekit_service.py
+++ tests/test_packagekit_service.py
@@ -11,52 +11,50 @@ gi.require_version("PackageKitGlib", "1.0")
 from gi.repository import PackageKitGlib
 
 
+def retrieve_package_id_from_name(name):
+    return PackageKitService().retrieve_package_information_by_name(name=name)[0].get("package_id")
+
+GLANCES_PACKAGE = "glances"
+FLASHBAKE_PACKAGE = "flashbake"
+
+
 class TestPackageKitCommonService(CommonServiceTests):
 
-    APPLICATION_TO_INSTALL_REMOVE = "glances"
-    SECOND_APPLICATION_INSTALL_REMOVE = "flashbake"
+    APPLICATION_TO_INSTALL_REMOVE = retrieve_package_id_from_name(name=GLANCES_PACKAGE)
+    SECOND_APPLICATION_INSTALL_REMOVE = retrieve_package_id_from_name(name=FLASHBAKE_PACKAGE)
+    PACKAGE_KIT_SERVICE = PackageKitService()
 
     PACKAGE_TYPE = "apt"
 
-    @staticmethod
-    def _retrieve_package_id_from_name(name):
-        return PackageKitService().retrieve_package_information_by_name(name=name)[0].get("package_id")
-
     def test_search_packages(self):
         package_name = "flat"
-        package_kit_service = PackageKitService()
-        result = package_kit_service.retrieve_package_information_by_name(name=package_name)
+        result = self.PACKAGE_KIT_SERVICE.retrieve_package_information_by_name(name=package_name)
         assert len(result)
         for package in result:
             self.assert_package_structure(package=package)
             assert package_name in package.get("name")
 
     def test_install_a_package(self):
-        package_id = self._retrieve_package_id_from_name(name=self.APPLICATION_TO_INSTALL_REMOVE)
-        package_kit_service = PackageKitService()
-        result = package_kit_service.install_package(name=package_id)
+        result = self.PACKAGE_KIT_SERVICE.install_package(name=self.APPLICATION_TO_INSTALL_REMOVE)
         assert result.get("message") == "success"
         assert result.get("action") == "install"
-        self.assert_installation_date(package_service=package_kit_service, expected_package_name=package_id)
+        self.assert_installation_date(package_service=self.PACKAGE_KIT_SERVICE, expected_package_name=self.APPLICATION_TO_INSTALL_REMOVE)
 
     def test_remove_a_package(self):
-        package_id = self._retrieve_package_id_from_name(name=self.APPLICATION_TO_INSTALL_REMOVE)
-        package_kit_service = PackageKitService()
-        result = package_kit_service.remove_package(name=package_id)
+        result = self.PACKAGE_KIT_SERVICE.remove_package(name=self.APPLICATION_TO_INSTALL_REMOVE)
         assert result.get("message") == "success"
         assert result.get("action") == "remove"
         self.assert_no_installation_date(
-            package_service=package_kit_service, expected_package_name=self.APPLICATION_TO_INSTALL_REMOVE
+            package_service=self.PACKAGE_KIT_SERVICE, expected_package_name=self.APPLICATION_TO_INSTALL_REMOVE
         )
 
     def test_remove_uninstalled_package(self):
-        package_id = self._retrieve_package_id_from_name(name=self.APPLICATION_TO_INSTALL_REMOVE)
         package_kit_service = PackageKitService()
-        result = package_kit_service.remove_package(name=package_id)
+        result = package_kit_service.remove_package(name=self.APPLICATION_TO_INSTALL_REMOVE)
         assert result.get("message") == "success"
         assert result.get("action") == "remove"
         self.assert_no_installation_date(
-            package_service=package_kit_service, expected_package_name=self.APPLICATION_TO_INSTALL_REMOVE
+            package_service=package_kit_service, expected_package_name=GLANCES_PACKAGE
         )
 
     def test_list_installed_packages(self):
