fed67f6bb4f90de740227f37f2c0d7267e084abb
Jeff Labonte <grimsleepless@protonmail.com>
2019-12-27 16:51:44 -0500

    Function to create a backup of sources.list
    The function and the test are together.

--- libboutique/ppa/source_handler.py
+++ libboutique/ppa/source_handler.py
@@ -1,5 +1,15 @@
-
+import shutil
 
 class SourceHandler:
-    SOURCE_LIST_PATH = "/etc/apt/source.list"
-    pass
+    SOURCE_LIST_PATH = "/etc/apt/sources.list"
+    SOURCE_LIST_BACKUP_PATH  = "/etc/apt/sources.list.back"
+
+    def __init__(self):
+        pass
+    
+    def _backup_source_file(self) -> None:
+        """
+            Back the source.list file in case
+            we need to do a rollback
+        """
+        shutil.copyfile(src=self.SOURCE_LIST_PATH, dst=self.SOURCE_LIST_BACKUP_PATH)
--- tests/test_ppa_source_handler.py
+++ tests/test_ppa_source_handler.py
@@ -1,3 +1,5 @@
+import os
+
 from libboutique.ppa.source_handler import SourceHandler
 
 
@@ -5,4 +7,18 @@ class TestSourceHandler:
 
     def test_init_source_handler(self):
         source_handler = SourceHandler()
-        assert source_handler.SOURCE_LIST_PATH == "/etc/apt/source.list"
+        assert source_handler.SOURCE_LIST_PATH == "/etc/apt/sources.list"
+        assert source_handler.SOURCE_LIST_BACKUP_PATH == source_handler.SOURCE_LIST_PATH + ".back"
+
+    def test_backup_source_list(self):
+        source_handler = SourceHandler()
+        if os.path.exists(source_handler.SOURCE_LIST_BACKUP_PATH):
+            os.remove(source_handler.SOURCE_LIST_BACKUP_PATH)
+        source_handler._backup_source_file()
+        assert os.path.exists(source_handler.SOURCE_LIST_BACKUP_PATH)
+        assert self._read_file_content(path=source_handler.SOURCE_LIST_PATH) == self._read_file_content(path=source_handler.SOURCE_LIST_BACKUP_PATH)
+
+    @staticmethod
+    def _read_file_content(path):
+        with open(path, "r") as f:
+            return f.readlines()
