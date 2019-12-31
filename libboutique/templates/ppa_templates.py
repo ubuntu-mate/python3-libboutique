e77792876de4585bd76de6d4e2d8331f552aecbb
Jeff Labonte <grimsleepless@protonmail.com>
2019-12-31 16:10:28 -0500

    Refactor template string name
    Change some of the name so they make more sense.

--- libboutique/templates/ppa_templates.py
+++ libboutique/templates/ppa_templates.py
@@ -1,1 +1,3 @@
-PPA_BASE_PATH = "/etc/apt/sources.list.d/{list_filename}"
+BASE_REPOSITORY_FILE_PATH = "/etc/apt/sources.list.d/{list_filename}"
+REPOSITORY_URL = "http://ppa.launchpad.net/{user}/{project}/ubuntu"
+CONTENT_PPA_REPOSITORY_FILE = "deb {url} {distro} main"
