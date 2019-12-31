018a2cc41bd9843efb85f65e7d3cfe841d5f119a
Jeff Labonte <grimsleepless@protonmail.com>
2019-12-31 04:45:14 -0500

    Document code

--- libboutique/formatters/ppa_formatter.py
+++ libboutique/formatters/ppa_formatter.py
@@ -3,11 +3,16 @@ from libboutique.templates import ppa_templates
 
 
 def format_list_file(user: str, project: str) -> str:
+    """
+        i.e: user: graphics, project: ppa -> graphics-ubuntu-ppa-eoan.list
+    """
     return f'{user}-{distro_wrapper.get_distro_id()}-{project}-{distro_wrapper.get_distro_codename()}.list'
 
 
-def format_file_path(user, project):
-    return ppa_templates.PPA_BASE_PATH.format(package=format_list_file(**locals()))
+def format_file_path(user: str, project: str) -> str:
+    """
+        i.e: /etc/apt/sources.list.d/graphics-ubuntu-ppa-eoan.list
+    """
 
 
 def format_file_content(url):
