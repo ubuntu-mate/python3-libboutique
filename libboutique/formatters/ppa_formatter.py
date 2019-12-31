8ec159343c8088e5db8147a4da5c22cd45b90618
Jeff Labonte <grimsleepless@protonmail.com>
2019-12-31 16:12:15 -0500

    Create a class to wrap all functions
    Using the class to allow me to not repeat myself.

--- libboutique/formatters/ppa_formatter.py
+++ libboutique/formatters/ppa_formatter.py
@@ -2,20 +2,39 @@ from libboutique.common import distro_wrapper
 from libboutique.templates import ppa_templates
 
 
-def format_list_filename(user: str, project: str) -> str:
+class PPAFormatter:
     """
-        i.e: user: graphics, project: ppa -> graphics-ubuntu-ppa-eoan.list
+        Takes care of formatting strings
+        that are required for handling ppas
     """
-    return f'{user}-{distro_wrapper.get_distro_id()}-{project}-{distro_wrapper.get_distro_codename()}.list'
 
+    def __init__(self, user: str, project: str):
+        self.user = user
+        self.project = project
 
-def format_file_path(user: str, project: str) -> str:
-    """
-        i.e: /etc/apt/sources.list.d/graphics-ubuntu-ppa-eoan.list
-    """
-    list_filename = format_list_filename(user=user, project=project)
-    return ppa_templates.PPA_BASE_PATH.format(list_filename=list_filename)
+    def format_list_filename(self) -> str:
+        """
+            i.e: user: graphics, project: ppa -> graphics-ubuntu-ppa-eoan.list
+        """
+        return f'{self.user}-{distro_wrapper.get_distro_id()}-{self.project}-{distro_wrapper.get_distro_codename()}.list'
 
+    def format_file_path(self) -> str:
+        """
+            i.e: /etc/apt/sources.list.d/graphics-ubuntu-ppa-eoan.list
+        """
+        list_filename = self.format_list_filename(user=self.user, project=self.project)
+        return ppa_templates.BASE_REPOSITORY_FILE_PATH.format(list_filename=list_filename)
 
-def format_file_content(url):
-    pass
+    def format_repository(self) -> str:
+        """
+            i.e: http://ppa.launchpad.net/graphics/ppa/ubuntu
+        """
+        return ppa_templates.LAUNCHPAD_URL.format(user=self.user, project=self.project)
+
+    def format_file_content(self):
+        """
+            i.e: deb http://ppa.launchpad.com/graphics/ppa/ubuntu eoan main
+        """
+        repository_url = self.format_repository()
+        return ppa_templates.CONTENT_PPA_REPOSITORY_FILE.format(url=repository_url,
+                                                                distro=distro_wrapper.get_distro_codename())
