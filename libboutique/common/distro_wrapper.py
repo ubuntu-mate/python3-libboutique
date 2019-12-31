d9417eb8f5f3ea6771f611dc61a68a40799b2867
Jeff Labonte <grimsleepless@protonmail.com>
2019-12-31 03:48:30 -0500

    Add id to distro wrapper

--- libboutique/common/distro_wrapper.py
+++ libboutique/common/distro_wrapper.py
@@ -12,3 +12,15 @@ def get_distro_codename() -> str:
         decorator @lru_cache
     """
     return LinuxDistribution().codename()
+
+
+@lru_cache()
+def get_distro_id() -> str:
+    """
+        Takes care of getting the distro id.
+
+        To reduce useless overhead, this function has the
+        decorator @lru_cache
+
+    """
+    return LinuxDistribution().id()
