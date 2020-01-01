83d3d9cf6a44aa0a3d73db2a7f6ea573bde43ae6
Jeff Labonte <grimsleepless@protonmail.com>
2020-01-01 02:49:48 -0500

    Create the tests for PPAFormatter

--- tests/formatters/test_ppa_formatter.py
+++ tests/formatters/test_ppa_formatter.py
@@ -9,6 +9,7 @@ class TestPPAFormatter:
 
     EXPECTED_LIST_FILENAME = f"graphics-team-ubuntu-ppa-{distro_wrapper.get_distro_codename()}.list"
     EXPECTED_FILE_PATH = f"/etc/apt/sources.list.d/graphics-team-ubuntu-ppa-{distro_wrapper.get_distro_codename()}.list"
+    EXPECTED_REPOSITORY = "http://ppa.launchpad.net/graphics-team/ppa/ubuntu"
 
     def __enter__(self):
         return PPAFormatter(self.USER, self.PROJECT)
@@ -17,11 +18,21 @@ class TestPPAFormatter:
         return False
 
     def test_format_list_filename(self):
-        ppa_formatter = PPAFormatter(user=self.USER, project=self.PROJECT)
-        list_filename = ppa_formatter.format_list_filename()
-        assert self.EXPECTED_LIST_FILENAME == list_filename
+        with self as ppa_formatter:
+            list_filename = ppa_formatter.format_list_filename()
+            assert self.EXPECTED_LIST_FILENAME == list_filename
 
     def test_format_file_path(self):
-        ppa_formatter = PPAFormatter(user=self.USER, project=self.PROJECT)
-        file_path = ppa_formatter.format_file_path()
-        assert self.EXPECTED_FILE_PATH == file_path
+        with self as ppa_formatter:
+            file_path = ppa_formatter.format_file_path()
+            assert self.EXPECTED_FILE_PATH == file_path
+
+    def test_format_repository(self):
+        with self as ppa_formatter:
+            repository = ppa_formatter.format_repository()
+            assert repository == self.EXPECTED_REPOSITORY
+
+    def test_format_file_content(self):
+        with self as ppa_formatter:
+            file_content = ppa_formatter.format_file_content()
+            assert file_content == f"deb {self.EXPECTED_REPOSITORY} {distro_wrapper.get_distro_codename()} main"
