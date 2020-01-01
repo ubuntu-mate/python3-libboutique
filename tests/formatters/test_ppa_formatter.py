6813afaff385a8d9c8c1c8523e1786813185d8ef
Jeff Labonte <grimsleepless@protonmail.com>
2020-01-01 02:20:23 -0500

    Create test on file_path in PPAFormatter

--- tests/formatters/test_ppa_formatter.py
+++ tests/formatters/test_ppa_formatter.py
@@ -8,8 +8,14 @@ class TestPPAFormatter:
     PROJECT = "ppa"
 
     EXPECTED_LIST_FILENAME = f"graphics-team-ubuntu-ppa-{distro_wrapper.get_distro_codename()}.list"
+    EXPECTED_FILE_PATH = f"/etc/apt/sources.list.d/graphics-team-ubuntu-ppa-{distro_wrapper.get_distro_codename()}.list"
 
     def test_format_list_filename(self):
         ppa_formatter = PPAFormatter(user=self.USER, project=self.PROJECT)
         list_filename = ppa_formatter.format_list_filename()
         assert self.EXPECTED_LIST_FILENAME == list_filename
+
+    def test_format_file_path(self):
+        ppa_formatter = PPAFormatter(user=self.USER, project=self.PROJECT)
+        file_path = ppa_formatter.format_file_path()
+        assert self.EXPECTED_FILE_PATH == file_path
