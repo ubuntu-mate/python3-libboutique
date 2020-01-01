02e28e347e4710a316a2462abe83e5b488ba0ded
Jeff Labonte <grimsleepless@protonmail.com>
2020-01-01 02:39:18 -0500

    Prepare context management to simplify code

--- tests/formatters/test_ppa_formatter.py
+++ tests/formatters/test_ppa_formatter.py
@@ -10,6 +10,12 @@ class TestPPAFormatter:
     EXPECTED_LIST_FILENAME = f"graphics-team-ubuntu-ppa-{distro_wrapper.get_distro_codename()}.list"
     EXPECTED_FILE_PATH = f"/etc/apt/sources.list.d/graphics-team-ubuntu-ppa-{distro_wrapper.get_distro_codename()}.list"
 
+    def __enter__(self):
+        return PPAFormatter(self.USER, self.PROJECT)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return False
+
     def test_format_list_filename(self):
         ppa_formatter = PPAFormatter(user=self.USER, project=self.PROJECT)
         list_filename = ppa_formatter.format_list_filename()
