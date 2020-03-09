71a8077140be5e142d549760837488aa50b1c237
Jeff Labonte <grimsleepless@protonmail.com>
2020-03-08 22:35:13 -0400

    Implement build_index in CuratedAppService

--- libboutique/services/curated_app_service.py
+++ libboutique/services/curated_app_service.py
@@ -1,3 +1,20 @@
+import pathlib
+import os
+import subprocess
+from functools import lru_cache
+
+
 class CuratedAppService:
-	def rebuild_index(self):
-		pass
+
+	DIST_CURATED_APPS_FOLDER = "libboutique/curated_apps/dist"
+	SCRIPT_CURATED_APPS_FOLDER = "libboutique/curated_apps/scripts"
+
+	def build_index(self, *_, **__):
+		command = ["sh", self._generate_path_to_build_index()]
+		return subprocess.check_output(command, stderr=subprocess.PIPE)
+
+	@lru_cache(maxsize=1)
+	def _generate_path_to_build_index(self):
+		return os.path.join(os.path.join(str(pathlib.Path.cwd()), self.SCRIPT_CURATED_APPS_FOLDER), "build.sh")
+
+
