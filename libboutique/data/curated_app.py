a56ef065f46e4a796107a7f1239da0076341d9ce
Jeff Labonte <jeff.labonte12@gmail.com>
2020-05-29 14:12:11 -0400

    WIP - Mapping metadata.json into a dataclass

--- libboutique/data/curated_app.py
+++ libboutique/data/curated_app.py
@@ -1,6 +1,20 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass(frozen=True)
 class CuratedApp:
-	name: str  # Name of the application, e.g. Brave
+    """
+    Dataclass to hold the JSON metadata into a read-only object
+    """
+    name: str  # Name of the application, e.g. Brave
+    listed: bool  # TODO Document the use of listed
+    summary: str  # A little summary of the application
+    developer_name: str
+    developer_url: str  # The developer website
+    description: str  # Complete description of the application
+    launch_cmd: str  # The command to use from the terminal
+    proprietary: bool
+    alternate_to: str = field(init=True)  # Always get init.
+    urls: dict  # More like links
+    arch: List  # List of architecture e.g. amd64, i386, arm
