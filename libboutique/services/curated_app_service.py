eaf90f07c35b71b6a789af32d8202e60b2f78923
Jeff Labonte <jeff.labonte12@gmail.com>
2020-06-02 00:52:53 -0400

    WIP Code

--- libboutique/services/curated_app_service.py
+++ libboutique/services/curated_app_service.py
@@ -1,6 +1,7 @@
 import pathlib
 import os
 import subprocess
+import json
 from functools import lru_cache
 
 
@@ -8,10 +9,17 @@ class CuratedAppService:
 
 	DIST_CURATED_APPS_FOLDER = "libboutique/curated_apps/dist"
 	SCRIPT_CURATED_APPS_FOLDER = "libboutique/curated_apps/scripts"
+	CURATED_APPS_APPLICATION_INDEX = "libboutique/curated_apps/dist/applications-en.json"
+
+	def __init__(self):
+		self._curated_apps_json = None
 
 	def build_index(self, *_, **__):
-		command = ["sh", self._generate_path_to_build_index()]
-		return subprocess.check_output(command, stderr=subprocess.PIPE)
+		if self._curated_apps_json is None:
+			command = ["sh", self._generate_path_to_build_index()]
+			if subprocess.check_output(command, stderr=subprocess.PIPE):
+				with open(self.CURATED_APPS_APPLICATION_INDEX, "r") as f:
+					self._curated_apps_json = json.load(f)
 
 	@lru_cache(maxsize=1)
 	def _generate_path_to_build_index(self):
